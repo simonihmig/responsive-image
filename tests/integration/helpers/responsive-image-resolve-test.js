8a245c65a633311df2e0b5878e55bf65390363cf
simonihmig <ihmig@kaliber5.de>
2018-02-21 14:10:15 +0100

    Migrate to new testing APIs

--- tests/integration/helpers/responsive-image-resolve-test.js
+++ tests/integration/helpers/responsive-image-resolve-test.js
@@ -1,28 +1,25 @@
-import { find } from 'ember-native-dom-helpers';
-/* jshint expr:true */
+import { find, render } from '@ember/test-helpers';
 import { expect } from 'chai';
 import {
   it,
   describe
 } from 'mocha';
-import { setupComponentTest } from 'ember-mocha';
+import { setupRenderingTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describe(
-  'Helper: ResponsiveImageResolve',
-  function() {
-    setupComponentTest('responsive-image', {
-      integration: true
-    });
-  it('works without size', function() {
-    this.render(hbs`{{responsive-image-resolve "test.png"}}`);
-    expect(find().innerHTML).to.equal("/assets/images/responsive/test100w-00e24234f1b58e32b935b1041432916f.png");
-  }),
-  it('is size aware', function() {
-    this.inject.service('responsive-image');
-    this.get('responsive-image').set('physicalWidth', 100);
-    this.render(hbs`{{responsive-image-resolve "test.png" 45}}`);
+describe('Helper: ResponsiveImageResolve', function() {
+  setupRenderingTest();
 
-    expect(find().innerHTML).to.equal("/assets/images/responsive/test50w-00e24234f1b58e32b935b1041432916f.png");
+  it('works without size', async function() {
+    await render(hbs`{{responsive-image-resolve "test.png"}}`);
+    expect(find('*').textContent.trim()).to.equal("/assets/images/responsive/test100w-00e24234f1b58e32b935b1041432916f.png");
+  });
+
+  it('is size aware', async function() {
+    let service = this.owner.lookup('service:responsive-image');
+    service.set('physicalWidth', 100);
+    await render(hbs`{{responsive-image-resolve "test.png" 45}}`);
+
+    expect(find('*').textContent.trim()).to.equal("/assets/images/responsive/test50w-00e24234f1b58e32b935b1041432916f.png");
   });
 });
