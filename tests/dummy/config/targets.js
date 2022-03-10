6cfba02bee16ec9dee66b6710125e077d075214e
simonihmig <simon.ihmig@kaliber5.de>
2022-03-10 20:40:28 +0100

    Fix targets

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -6,17 +6,7 @@ const browsers = [
   'last 1 Safari versions',
 ];
 
-// check if we have https://github.com/emberjs/ember.js/pull/19397
-const semver = require('semver');
-const supportsNodeTargets = semver.gte(
-  require('ember-source/package.json').version,
-  '3.27.0'
-);
-
-// ember-auto-import v2 expects a `browsers` property, so use the comman targets format when supported
-module.exports = supportsNodeTargets
-  ? {
-      browsers,
-      node: true,
-    }
-  : [...browsers, 'maintained node versions'];
+module.exports = {
+  browsers,
+  node: true,
+};
