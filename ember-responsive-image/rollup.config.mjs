10796c15e6d4f4a4d654ab735cb5fe95a5e3866d
simonihmig <simon.ihmig@kaliber5.de>
2022-10-18 18:17:08 +0200

    Start work on loader

--- ember-responsive-image/rollup.config.mjs
+++ ember-responsive-image/rollup.config.mjs
@@ -14,7 +14,7 @@ export default {
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints(['components/**/*.ts', 'helpers/**/*.ts', 'services/**/*.ts', 'index.ts', 'glint.ts', 'types.ts']),
+    addon.publicEntrypoints(['components/**/*.ts', 'helpers/**/*.ts', 'services/**/*.ts', 'index.ts', 'glint.ts', 'types.ts', 'loader/index.ts']),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
