b35e75348386b50d61e4d6257f7428667abcd525
Simon Ihmig <simon.ihmig@kaliber5.de>
2023-03-25 16:20:21 +0100

    Skip FastBoot tests for Ember 5

--- tests/fastboot/cloudinary-test.js
+++ tests/fastboot/cloudinary-test.js
@@ -3,16 +3,24 @@ import {
   setup,
   visit /* mockServer */,
 } from 'ember-cli-fastboot-testing/test-support';
+import { dependencySatisfies } from '@embroider/macros';
 
-module('FastBoot | Cloudinary', function (hooks) {
-  setup(hooks);
+// FastBoot does not not suppport Ember 5 yet :-(
+(dependencySatisfies('ember-source', '>= 5') ? module.skip : module)(
+  'FastBoot | Cloudinary',
+  function (hooks) {
+    setup(hooks);
 
-  test('it renders an image', async function (assert) {
-    await visit('/cloudinary');
+    test('it renders an image', async function (assert) {
+      await visit('/cloudinary');
 
-    assert.dom('img[data-test-image]').exists();
-    assert
-      .dom('img[data-test-image]')
-      .hasAttribute('src', new RegExp('https://res.cloudinary.com/kaliber5/'));
-  });
-});
+      assert.dom('img[data-test-image]').exists();
+      assert
+        .dom('img[data-test-image]')
+        .hasAttribute(
+          'src',
+          new RegExp('https://res.cloudinary.com/kaliber5/')
+        );
+    });
+  }
+);
