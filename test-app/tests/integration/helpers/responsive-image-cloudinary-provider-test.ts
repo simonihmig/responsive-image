6e50873ecf61a7b8ead1babf09772093bd3da597
Simon Ihmig <simon.ihmig@kaliber5.de>
2023-12-17 12:28:02 +0100

    Refactor dump helper for embroider compat

--- test-app/tests/integration/helpers/responsive-image-cloudinary-provider-test.ts
+++ test-app/tests/integration/helpers/responsive-image-cloudinary-provider-test.ts
@@ -1,130 +1,123 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, type RenderingTestContext } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import setupDataDumper from 'test-app/tests/helpers/dump';
 import type { ProviderResult } from 'ember-responsive-image/types';
+
+interface TestContext extends RenderingTestContext {
+  dump: (argument: ProviderResult) => void;
+}
 
 module(
   'Integration | Helper | responsive-image-cloudinary-provider',
   function (hooks) {
     setupRenderingTest(hooks);
-    const getData = setupDataDumper(hooks);
 
-    test('it supports all image types', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-cloudinary-provider "samples/animals/three-dogs")}}`,
-      );
-
-      const data = getData() as ProviderResult;
-
-      assert.deepEqual(data.imageTypes, ['png', 'jpeg', 'webp', 'avif']);
+    let data: ProviderResult | undefined;
+    hooks.before(function (this: TestContext) {
+      this.dump = (argument: ProviderResult) => {
+        data = argument;
+      };
     });
 
-    test('it returns correct upload image URLs', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-cloudinary-provider "samples/animals/three-dogs")}}`,
+    test('it supports all image types', async function (this: TestContext, assert) {
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-cloudinary-provider "samples/animals/three-dogs")}}`,
       );
 
-      const data = getData() as ProviderResult;
+      assert.deepEqual(data?.imageTypes, ['png', 'jpeg', 'webp', 'avif']);
+    });
+
+    test('it returns correct upload image URLs', async function (this: TestContext, assert) {
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-cloudinary-provider "samples/animals/three-dogs")}}`,
+      );
 
       assert.strictEqual(
-        data.imageUrlFor(100, 'jpeg'),
+        data?.imageUrlFor(100, 'jpeg'),
         'https://res.cloudinary.com/kaliber5/image/upload/w_100,c_limit,q_auto/samples/animals/three-dogs.jpeg',
       );
 
       assert.strictEqual(
-        data.imageUrlFor(1000, 'jpeg'),
+        data?.imageUrlFor(1000, 'jpeg'),
         'https://res.cloudinary.com/kaliber5/image/upload/w_1000,c_limit,q_auto/samples/animals/three-dogs.jpeg',
       );
 
       assert.strictEqual(
-        data.imageUrlFor(100, 'webp'),
+        data?.imageUrlFor(100, 'webp'),
         'https://res.cloudinary.com/kaliber5/image/upload/w_100,c_limit,q_auto/samples/animals/three-dogs.webp',
       );
     });
 
-    test('it returns correct fetch image URLs', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-cloudinary-provider "https://via.placeholder.com/150")}}`,
+    test('it returns correct fetch image URLs', async function (this: TestContext, assert) {
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-cloudinary-provider "https://via.placeholder.com/150")}}`,
       );
 
-      const data = getData() as ProviderResult;
-
       assert.strictEqual(
-        data.imageUrlFor(100, 'jpeg'),
+        data?.imageUrlFor(100, 'jpeg'),
         'https://res.cloudinary.com/kaliber5/image/fetch/w_100,c_limit,q_auto,f_jpg/https%3A%2F%2Fvia.placeholder.com%2F150',
       );
 
       assert.strictEqual(
-        data.imageUrlFor(1000, 'jpeg'),
+        data?.imageUrlFor(1000, 'jpeg'),
         'https://res.cloudinary.com/kaliber5/image/fetch/w_1000,c_limit,q_auto,f_jpg/https%3A%2F%2Fvia.placeholder.com%2F150',
       );
 
       assert.strictEqual(
-        data.imageUrlFor(100, 'webp'),
+        data?.imageUrlFor(100, 'webp'),
         'https://res.cloudinary.com/kaliber5/image/fetch/w_100,c_limit,q_auto,f_webp/https%3A%2F%2Fvia.placeholder.com%2F150',
       );
     });
 
-    test('it supports custom transformations', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-cloudinary-provider "samples/animals/three-dogs" transformations="co_rgb:20a020,e_colorize:50")}}`,
+    test('it supports custom transformations', async function (this: TestContext, assert) {
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-cloudinary-provider "samples/animals/three-dogs" transformations="co_rgb:20a020,e_colorize:50")}}`,
       );
 
-      const data = getData() as ProviderResult;
-
       assert.strictEqual(
-        data.imageUrlFor(100, 'jpeg'),
+        data?.imageUrlFor(100, 'jpeg'),
         'https://res.cloudinary.com/kaliber5/image/upload/co_rgb:20a020,e_colorize:50/w_100,c_limit,q_auto/samples/animals/three-dogs.jpeg',
       );
     });
 
-    test('it supports custom chained transformations', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-cloudinary-provider "samples/animals/three-dogs" transformations="co_rgb:20a020,e_colorize:50/ar_1.0,c_fill,w_150/r_max")}}`,
+    test('it supports custom chained transformations', async function (this: TestContext, assert) {
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-cloudinary-provider "samples/animals/three-dogs" transformations="co_rgb:20a020,e_colorize:50/ar_1.0,c_fill,w_150/r_max")}}`,
       );
 
-      const data = getData() as ProviderResult;
-
       assert.strictEqual(
-        data.imageUrlFor(100, 'jpeg'),
+        data?.imageUrlFor(100, 'jpeg'),
         'https://res.cloudinary.com/kaliber5/image/upload/co_rgb:20a020,e_colorize:50/ar_1.0,c_fill,w_150/r_max/w_100,c_limit,q_auto/samples/animals/three-dogs.jpeg',
       );
     });
 
-    test('it supports custom image formats', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-cloudinary-provider "samples/animals/three-dogs" formats=(array "webp" "avif"))}}`,
+    test('it supports custom image formats', async function (this: TestContext, assert) {
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-cloudinary-provider "samples/animals/three-dogs" formats=(array "webp" "avif"))}}`,
       );
 
-      const data = getData() as ProviderResult;
-
-      assert.deepEqual(data.imageTypes, ['webp', 'avif']);
+      assert.deepEqual(data?.imageTypes, ['webp', 'avif']);
     });
 
-    test('it supports custom quality setting', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-cloudinary-provider "samples/animals/three-dogs" quality=50)}}`,
+    test('it supports custom quality setting', async function (this: TestContext, assert) {
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-cloudinary-provider "samples/animals/three-dogs" quality=50)}}`,
       );
 
-      const data = getData() as ProviderResult;
-
       assert.strictEqual(
-        data.imageUrlFor(100, 'jpeg'),
+        data?.imageUrlFor(100, 'jpeg'),
         'https://res.cloudinary.com/kaliber5/image/upload/w_100,c_limit,q_50/samples/animals/three-dogs.jpeg',
       );
     });
 
-    test('it supports remote fetching', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-cloudinary-provider "https://www.example.com/image.jpg")}}`,
+    test('it supports remote fetching', async function (this: TestContext, assert) {
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-cloudinary-provider "https://www.example.com/image.jpg")}}`,
       );
 
-      const data = getData() as ProviderResult;
-
       assert.strictEqual(
-        data.imageUrlFor(100, 'webp'),
+        data?.imageUrlFor(100, 'webp'),
         'https://res.cloudinary.com/kaliber5/image/fetch/w_100,c_limit,q_auto,f_webp/https%3A%2F%2Fwww.example.com%2Fimage.jpg',
       );
     });
--- test-app/tests/integration/helpers/responsive-image-imgix-provider-test.ts
+++ test-app/tests/integration/helpers/responsive-image-imgix-provider-test.ts
@@ -1,81 +1,79 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, type RenderingTestContext } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import setupDataDumper from 'test-app/tests/helpers/dump';
 import type { ProviderResult } from 'ember-responsive-image/types';
+
+interface TestContext extends RenderingTestContext {
+  dump: (argument: ProviderResult) => void;
+}
 
 module(
   'Integration | Helper | responsive-image-imgix-provider',
   function (hooks) {
     setupRenderingTest(hooks);
-    const getData = setupDataDumper(hooks);
+    let data: ProviderResult | undefined;
+    hooks.before(function (this: TestContext) {
+      this.dump = (argument: ProviderResult) => {
+        data = argument;
+      };
+    });
 
     test('it supports jpg, png and webp image types', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-imgix-provider "foo/bar.jpg")}}`,
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-imgix-provider "foo/bar.jpg")}}`,
       );
 
-      const data = getData() as ProviderResult;
-
-      assert.deepEqual(data.imageTypes, ['png', 'jpeg', 'webp']);
+      assert.deepEqual(data?.imageTypes, ['png', 'jpeg', 'webp']);
     });
 
     test('it returns correct image URLs', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-imgix-provider "foo/bar.jpg")}}`,
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-imgix-provider "foo/bar.jpg")}}`,
       );
 
-      const data = getData() as ProviderResult;
-
       assert.strictEqual(
-        data.imageUrlFor(100, 'jpeg'),
+        data?.imageUrlFor(100, 'jpeg'),
         'https://kaliber5.imgix.net/foo/bar.jpg?fm=jpg&w=100&fit=max',
       );
 
       assert.strictEqual(
-        data.imageUrlFor(1000, 'jpeg'),
+        data?.imageUrlFor(1000, 'jpeg'),
         'https://kaliber5.imgix.net/foo/bar.jpg?fm=jpg&w=1000&fit=max',
       );
 
       assert.strictEqual(
-        data.imageUrlFor(100, 'webp'),
+        data?.imageUrlFor(100, 'webp'),
         'https://kaliber5.imgix.net/foo/bar.jpg?fm=webp&w=100&fit=max',
       );
     });
 
     test('it supports custom params', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-imgix-provider "foo/bar.jpg" params=(hash monochrome="44768B" px=10))}}`,
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-imgix-provider "foo/bar.jpg" params=(hash monochrome="44768B" px=10))}}`,
       );
 
-      const data = getData() as ProviderResult;
-
       assert.strictEqual(
-        data.imageUrlFor(100, 'jpeg'),
+        data?.imageUrlFor(100, 'jpeg'),
         'https://kaliber5.imgix.net/foo/bar.jpg?fm=jpg&w=100&fit=max&monochrome=44768B&px=10',
       );
     });
 
     test('it supports custom image formats', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-imgix-provider "foo/bar.jpg" formats=(array "webp" "jpeg"))}}`,
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-imgix-provider "foo/bar.jpg" formats=(array "webp" "jpeg"))}}`,
       );
 
-      const data = getData() as ProviderResult;
-
-      assert.deepEqual(data.imageTypes, ['webp', 'jpeg']);
+      assert.deepEqual(data?.imageTypes, ['webp', 'jpeg']);
     });
 
     test('it supports custom quality setting', async function (assert) {
-      await render(
-        hbs`{{dump (responsive-image-imgix-provider "foo/bar.jpg" quality=50)}}`,
+      await render<TestContext>(
+        hbs`{{this.dump (responsive-image-imgix-provider "foo/bar.jpg" quality=50)}}`,
       );
 
-      const data = getData() as ProviderResult;
-
       assert.strictEqual(
-        data.imageUrlFor(100, 'jpeg'),
+        data?.imageUrlFor(100, 'jpeg'),
         'https://kaliber5.imgix.net/foo/bar.jpg?fm=jpg&w=100&fit=max&q=50',
       );
     });
