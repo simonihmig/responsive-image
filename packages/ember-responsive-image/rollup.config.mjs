28c51a49ebff5dbe19f0911089e6725377cc26b1
Simon Ihmig <simon.ihmig@kaliber5.de>
2023-12-31 18:06:43 +0100

    Update eri addon to latest blueprint boilerplate

--- packages/ember-responsive-image/rollup.config.mjs
+++ packages/ember-responsive-image/rollup.config.mjs
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-ts';
+import { babel } from '@rollup/plugin-babel';
 import copy from 'rollup-plugin-copy';
 import { Addon } from '@embroider/addon-dev/rollup';
 
@@ -32,11 +32,15 @@ export default {
       'services/**/*.js',
     ]),
 
-    // compile TypeScript to latest JavaScript, including Babel transpilation
-    typescript({
-      transpiler: 'babel',
-      browserslist: false,
-      transpileOnly: false,
+    // This babel config should *not* apply presets or compile away ES modules.
+    // It exists only to provide development niceties for you, like automatic
+    // template colocation.
+    //
+    // By default, this will load the actual babel config from the file
+    // babel.config.json.
+    babel({
+      extensions: ['.js', '.gjs', '.ts', '.gts'],
+      babelHelpers: 'bundled',
     }),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
@@ -46,6 +50,9 @@ export default {
 
     // Ensure that standalone .hbs files are properly integrated as Javascript.
     addon.hbs(),
+
+    // Ensure that .gjs files are properly integrated as Javascript
+    addon.gjs(),
 
     // addons are allowed to contain imports of .css files, which we want rollup
     // to leave alone and keep in the published output.
@@ -57,7 +64,7 @@ export default {
     // Copy Readme and License into published package
     copy({
       targets: [
-        // { src: '../../README.md', dest: '.' },
+        { src: '../../README.md', dest: '.' },
         { src: '../../LICENSE.md', dest: '.' },
       ],
     }),
--- packages/ember-responsive-image/src/index.ts
+++ packages/ember-responsive-image/src/index.ts
@@ -1,5 +1,5 @@
-export { default as ResponsiveImage } from './components/responsive-image';
-export { default as ResponsiveImageService } from './services/responsive-image';
-export { default as resolve } from './helpers/responsive-image-resolve';
-export * from './types';
-export { findMatchingImage } from './utils/match';
+export { default as ResponsiveImage } from './components/responsive-image.ts';
+export { default as ResponsiveImageService } from './services/responsive-image.ts';
+export { default as resolve } from './helpers/responsive-image-resolve.ts';
+export * from './types.ts';
+export { findMatchingImage } from './utils/match.ts';
