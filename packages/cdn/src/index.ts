6d5406c1deeac3b68a9f15e7ca873dbdecbf7e88
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-04-29 22:24:41 +0200

    Add cdn package

---
+++ packages/cdn/src/index.ts
@@ -0,0 +1,2 @@
+export { provider as cloudinary } from './cloudinary.ts';
+export { provider as imgix } from './imgix.ts';
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,6 @@
 export { findMatchingImage } from './match.ts';
 export * from './env.ts';
 export * from './config.ts';
+export * from './debug.ts';
 
 export type * from './types.ts';
