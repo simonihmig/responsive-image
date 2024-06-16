4cb707b707b0c30a8c1441e7eff7d72d5f3edf91
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-06-16 14:11:45 +0200

    Scaffold ember-vite app

---
+++ test-apps/ember-vite/ember-cli-build.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { maybeEmbroider } = require('@embroider/test-setup');
+
+module.exports = function (defaults) {
+  let app = new EmberApp(defaults, {});
+
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
+};
