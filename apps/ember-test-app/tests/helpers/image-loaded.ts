658f0ea5744f560b0b27ca6f402e44466f28298d
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-10-21 20:10:31 +0200

    reject error

--- apps/ember-test-app/tests/helpers/image-loaded.ts
+++ apps/ember-test-app/tests/helpers/image-loaded.ts
@@ -3,15 +3,31 @@ import { modifier } from 'ember-modifier';
 
 export function imageLoaded() {
   let resolve: (v: unknown) => void;
-  const loaded = new Promise((r) => {
-    resolve = r;
+  let reject: (e: unknown) => void;
+  const loaded = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
   }).then(settled);
 
   const onload = modifier((el: HTMLImageElement) => {
     if (el.complete) {
       resolve(undefined);
     } else {
-      el.addEventListener('load', () => setTimeout(resolve, 0), { once: true });
+      el.addEventListener(
+        'load',
+        () => {
+          setTimeout(resolve, 0);
+        },
+        { once: true },
+      );
+      el.addEventListener(
+        'error',
+        (e) => {
+          console.error(e.message);
+          reject(e);
+        },
+        { once: true },
+      );
     }
   });
 
