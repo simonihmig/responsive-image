73e2c2722595488298abf927e966aee0f3e500bc
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-11-10 21:47:10 +0100

    Fix TS setup in ember-webpack

--- apps/ember-webpack/ember-cli-build.js
+++ apps/ember-webpack/ember-cli-build.js
@@ -6,7 +6,11 @@ const { setupLoaders } = require('@responsive-image/webpack');
 const EmberResponsiveImageWebpackLoaders = setupLoaders();
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {});
+  let app = new EmberApp(defaults, {
+    'ember-cli-babel': {
+      enableTypeScriptTransform: true,
+    },
+  });
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
