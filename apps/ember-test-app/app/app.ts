5d6798e47f471bf63ac9e8d3338801037643d647
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-11-10 16:33:32 +0100

    Use explicit setConfig in apps

--- apps/ember-test-app/app/app.ts
+++ apps/ember-test-app/app/app.ts
@@ -2,11 +2,19 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-test-app/config/environment';
+import { setConfig } from '@responsive-image/core';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
 }
+
+setConfig('cloudinary', {
+  cloudName: 'kaliber5',
+});
+setConfig('imgix', {
+  domain: 'kaliber5.imgix.net',
+});
 
 loadInitializers(App, config.modulePrefix);
--- apps/ember-vite/app/app.js
+++ apps/ember-vite/app/app.js
@@ -3,11 +3,19 @@ import compatModules from '@embroider/core/entrypoint';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import { setConfig } from '@responsive-image/core';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver.withModules(compatModules);
 }
+
+setConfig('cloudinary', {
+  cloudName: 'kaliber5',
+});
+setConfig('imgix', {
+  domain: 'kaliber5.imgix.net',
+});
 
 loadInitializers(App, config.modulePrefix, compatModules);
