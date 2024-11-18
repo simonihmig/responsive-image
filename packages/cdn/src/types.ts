3f9bd781768c8fa554eb1348072c94df63a37145
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-11-18 22:36:26 +0100

    Add new netlify image CDN provider

--- packages/cdn/src/types.ts
+++ packages/cdn/src/types.ts
@@ -1,7 +1,9 @@
 import { CloudinaryConfig } from './cloudinary';
 import { ImgixConfig } from './imgix';
+import { NetlifyConfig } from './netlify';
 
 export interface Config {
   imgix?: ImgixConfig;
   cloudinary?: CloudinaryConfig;
+  netlify?: NetlifyConfig;
 }
