779c9ae3c676b9a9b91801f20e1573ebaa64613b
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-12-15 18:53:56 +0100

    Cleanup solid-start app

--- apps/solid-start/src/app.tsx
+++ apps/solid-start/src/app.tsx
@@ -1,17 +1,15 @@
-import { MetaProvider, Title } from "@solidjs/meta";
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import "./app.css";
+import { MetaProvider, Title } from '@solidjs/meta';
+import { Router } from '@solidjs/router';
+import { FileRoutes } from '@solidjs/start/router';
+import { Suspense } from 'solid-js';
+import './app.css';
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
+          <Title>SolidStart - ResponsiveImage</Title>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
--- apps/solid-start/src/routes/index.tsx
+++ apps/solid-start/src/routes/index.tsx
@@ -1,19 +1,9 @@
-import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import { Title } from '@solidjs/meta';
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <h1>ResponsiveImage for Solid</h1>
     </main>
   );
 }
