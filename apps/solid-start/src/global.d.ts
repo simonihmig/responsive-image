ed9f358727200c5b46661b7ec2a9a703c13fbeda
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-12-20 11:49:16 +0100

    Add solid-start demo app contents

--- apps/solid-start/src/global.d.ts
+++ apps/solid-start/src/global.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="@solidjs/start/env" />
+
+// To make TS understand our image imports, we have to tag them using a `responsive` query parameter, that has to come *last*!
+// We cannot use something like `*.jpg*` that works with queries, as TS only supports a single wildcard.
+// See https://github.com/microsoft/TypeScript/issues/38638
+declare module '*responsive' {
+  import { ImageData } from '@responsive-image/core';
+  const value: ImageData;
+  export default value;
+}
