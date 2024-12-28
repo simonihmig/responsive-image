b8a8961b12a8f8cd9cd4018d9eb032826574647d
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-12-29 00:16:38 +0100

    Add ThumbHash support to webpack

---
+++ packages/webpack/src/lqip/thumbhash.ts
@@ -0,0 +1,79 @@
+import type { Metadata } from 'sharp';
+import type { LoaderContext } from 'webpack';
+import type { Options } from '../types';
+import { getWebpackOptions } from '../utils';
+import {
+  getAspectRatio,
+  type ImageLoaderChainedResult,
+  normalizeInput,
+} from '@responsive-image/build-utils';
+import { rgbaToThumbHash } from 'thumbhash';
+
+export default function lqipThumbhashLoader(
+  this: LoaderContext<Partial<Options>>,
+  input: Buffer | ImageLoaderChainedResult,
+) {
+  const data = normalizeInput(input);
+  const options = getWebpackOptions(this);
+  if (options.lqip?.type !== 'thumbhash') {
+    return data;
+  }
+
+  const loaderCallback = this.async();
+
+  process(data, options)
+    .then((result) => {
+      // @ts-expect-error wrong webpack types
+      loaderCallback(null, result);
+    })
+    .catch((err) => loaderCallback(err));
+}
+
+lqipThumbhashLoader.raw = true;
+
+async function process(
+  data: ImageLoaderChainedResult,
+  options: Options,
+): Promise<ImageLoaderChainedResult> {
+  const { sharp, sharpMeta } = data;
+
+  if (!sharpMeta) {
+    throw new Error('Expected sharp metadata to be available');
+  }
+
+  const { width, height } = await getLqipInputDimensions(sharpMeta);
+  const lqi = sharp
+    .clone()
+    .ensureAlpha()
+    .resize(width, height, {
+      fit: 'fill',
+    })
+    .raw();
+
+  const imageData = new Uint8ClampedArray(await lqi.toBuffer());
+  const rawHash = rgbaToThumbHash(width, height, imageData);
+
+  return {
+    ...data,
+    lqip: {
+      type: 'thumbhash',
+      hash: Buffer.from(rawHash).toString('base64'),
+    },
+  };
+}
+
+function getLqipInputDimensions(meta: Metadata) {
+  const aspectRatio = getAspectRatio(meta) ?? 1;
+  let width: number;
+  let height: number;
+
+  if (aspectRatio > 1) {
+    width = 100;
+    height = Math.round(100 / aspectRatio);
+  } else {
+    width = Math.round(100 * aspectRatio);
+    height = 100;
+  }
+
+  return { width, height };
+}
