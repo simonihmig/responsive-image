3f1ce8786fdcd7af18fe5452aa6a49ba0ceb3fe5
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-12-29 13:55:47 +0100

    Add ThumbHash FastBoot test

--- apps/ember-test-app/app/controllers/image.ts
+++ apps/ember-test-app/app/controllers/image.ts
@@ -3,12 +3,14 @@ import testImage from 'ember-test-app/images/aurora.jpg?widths=50,100,640&format
 import testImageLqipInline from 'ember-test-app/images/aurora.jpg?lqip=inline&widths=50,100,640&responsive';
 import testImageLqipColor from 'ember-test-app/images/aurora.jpg?lqip=color&widths=50,100,640&responsive';
 import testImageLqipBlurhash from 'ember-test-app/images/aurora.jpg?lqip=blurhash&widths=50,100,640&responsive';
+import testImageLqipThumbhash from 'ember-test-app/images/aurora.jpg?lqip=thumbhash&widths=50,100,640&responsive';
 
 export default class IndexController extends Controller {
   testImage = testImage;
   testImageLqipInline = testImageLqipInline;
   testImageLqipColor = testImageLqipColor;
   testImageLqipBlurhash = testImageLqipBlurhash;
+  testImageLqipThumbhash = testImageLqipThumbhash;
 }
 
 export const raw = true;
