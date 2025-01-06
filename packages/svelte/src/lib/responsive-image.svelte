6bcfec2df1304ec110a1cbc284273477506ca6e8
Simon Ihmig <simon.ihmig@kaliber5.de>
2025-01-06 11:57:36 +0100

    Basic Svelte implementation

--- packages/svelte/src/lib/responsive-image.svelte
+++ packages/svelte/src/lib/responsive-image.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
-	import type { ImageType, ImageData } from '@responsive-image/core';
+	import {
+		type ImageType,
+		type ImageData,
+		getDestinationWidthBySize,
+		env
+	} from '@responsive-image/core';
 	import type { HTMLImgAttributes } from 'svelte/elements';
 
-	let { src, size, sizes, width, height, ...htmlAttributes }: ResponsiveImageProps = $props();
+	let {
+		src: srcProp,
+		size: sizeProp,
+		sizes: sizesProp,
+		width: widthProp,
+		height: heightProp,
+		...htmlAttributes
+	}: ResponsiveImageProps = $props();
 
 	interface ResponsiveImageArgs {
 		src: ImageData;
@@ -32,8 +44,105 @@
 		['webp', 2],
 		['avif', 3]
 	]);
+
+	let isLoaded = $state(false);
+
+	const isResponsiveLayout = widthProp === undefined && heightProp === undefined;
+
+	let width: number | undefined = undefined;
+
+	if (isResponsiveLayout) {
+		width = getDestinationWidthBySize(sizeProp);
+	} else {
+		if (widthProp) {
+			width = widthProp;
+		} else {
+			const ar = srcProp.aspectRatio;
+			if (ar !== undefined && ar !== 0 && heightProp !== undefined) {
+				width = heightProp * ar;
+			}
+		}
+	}
+
+	let height: number | undefined = heightProp;
+
+	const ar = srcProp.aspectRatio;
+	if (height === undefined && ar !== undefined && ar !== 0 && width !== undefined) {
+		height = Math.round(width / ar);
+	}
+
+	const src = srcProp.imageUrlFor(width ?? 640);
+	let sources: ImageSource[];
+
+	if (isResponsiveLayout) {
+		sources = srcProp.imageTypes.map((type) => {
+			let widths = srcProp.availableWidths;
+			if (!widths) {
+				widths = env.deviceWidths;
+			}
+			const sources: string[] = widths.map((width) => {
+				const url = srcProp.imageUrlFor(width, type);
+				return `${url} ${width}w`;
+			});
+
+			return {
+				srcset: sources.join(', '),
+				sizes: sizesProp ?? (sizeProp ? `${sizeProp}vw` : undefined),
+				type,
+				mimeType: `image/${type}`
+			};
+		});
+	} else {
+		if (width === undefined) {
+			sources = [];
+		} else {
+			sources = srcProp.imageTypes.map((type) => {
+				const sources: string[] = PIXEL_DENSITIES.map((density) => {
+					// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+					const url = srcProp.imageUrlFor(width * density, type)!;
+
+					return `${url} ${density}x`;
+				}).filter((source) => source !== undefined);
+
+				return {
+					srcset: sources.join(', '),
+					type,
+					mimeType: `image/${type}`
+				};
+			});
+		}
+	}
+
+	const sourcesSorted = sources.sort(
+		(a, b) => (typeScore.get(b.type) ?? 0) - (typeScore.get(a.type) ?? 0)
+	);
+
+	const classNames = $derived.by(() => {
+		const classNames = [`ri-${isResponsiveLayout ? 'responsive' : 'fixed'}`];
+		const lqip = srcProp.lqip;
+		if (lqip && !isLoaded) {
+			classNames.push(`ri-lqip-${lqip.type}`);
+			if (lqip.type === 'color' || lqip.type === 'inline') {
+				classNames.push(lqip.class);
+			}
+		}
+
+		return classNames;
+	});
 </script>
 
 <picture>
-	<img loading="lazy" decoding="async" src="" alt="" {...htmlAttributes} />
+	{#each sourcesSorted as s}
+		<source srcset={s.srcset} type={s.mimeType} sizes={s.sizes} />
+	{/each}
+	<img
+		{width}
+		{height}
+		loading="lazy"
+		decoding="async"
+		{src}
+		alt=""
+		class={classNames.join(' ')}
+		{...htmlAttributes}
+	/>
 </picture>
