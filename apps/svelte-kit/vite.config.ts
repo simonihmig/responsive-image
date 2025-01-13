488490c95e74ca6e65653f172d3944a2de5ce593
Simon Ihmig <simon.ihmig@kaliber5.de>
2025-01-13 22:08:51 +0100

    Add demo images to svelte-kit app

--- apps/svelte-kit/vite.config.ts
+++ apps/svelte-kit/vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { setupPlugins } from '@responsive-image/vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    setupPlugins({
+      include: /^[^?]+\.jpg\?.*responsive.*$/,
+    }),
+  ],
 });
