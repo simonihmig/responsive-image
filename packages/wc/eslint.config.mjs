38982c077a1d738d3c8c3e60e01109851ca06142
Simon Ihmig <simon.ihmig@kaliber5.de>
2025-01-18 14:50:03 +0100

    Use ESLint v9 and flat config for wc package

---
+++ packages/wc/eslint.config.mjs
@@ -0,0 +1,44 @@
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
+import n from 'eslint-plugin-n';
+import { configs } from 'eslint-plugin-lit';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default ts.config(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  configs['flat/recommended'],
+  prettier,
+  {
+    files: ['src/**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  // node files
+  {
+    files: ['eslint.config.mjs', 'rollup.config.mjs', 'vitest.config.ts'],
+    plugins: {
+      n,
+    },
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
+);
