f6529e85ad6458cc5df20712f4f66ef4b443ec24
Simon Ihmig <simon.ihmig@kaliber5.de>
2025-06-04 23:32:34 +0200

    Refactor env to getters for better testability, add tests

--- packages/core/src/env.ts
+++ packages/core/src/env.ts
@@ -2,23 +2,27 @@ import { getConfig } from './config.ts';
 
 import type { Env, EnvConfig } from './types';
 
-const screenWidth = typeof screen !== 'undefined' ? screen.width : 320;
-const devicePixelRatio =
-  typeof window !== 'undefined' ? (window?.devicePixelRatio ?? 1) : 1;
-const physicalWidth = screenWidth * devicePixelRatio;
-const deviceWidths = getConfig<EnvConfig>('env')?.deviceWidths ?? [
-  640, 750, 828, 1080, 1200, 1920, 2048, 3840,
-];
-
 export const env: Env = {
-  screenWidth,
-  physicalWidth,
-  devicePixelRatio,
-  deviceWidths,
+  get screenWidth(): number {
+    return typeof screen !== 'undefined' ? screen.width : 320;
+  },
+  get physicalWidth(): number {
+    return env.screenWidth * env.devicePixelRatio;
+  },
+  get devicePixelRatio(): number {
+    return typeof window !== 'undefined' ? (window?.devicePixelRatio ?? 1) : 1;
+  },
+  get deviceWidths(): number[] {
+    return (
+      getConfig<EnvConfig>('env')?.deviceWidths ?? [
+        640, 750, 828, 1080, 1200, 1920, 2048, 3840,
+      ]
+    );
+  },
 };
 
 export function getDestinationWidthBySize(size = 100): number {
   const factor = size / 100;
 
-  return physicalWidth * factor;
+  return env.physicalWidth * factor;
 }
