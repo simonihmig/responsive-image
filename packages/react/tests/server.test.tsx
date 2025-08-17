b1df5ff21c830f09c0657f1fc0af6fcfa0f59abe
William Killerud <william@killerud.com>
2025-08-17 15:30:23 +0200

    Add a unit test for fallbackScreenWidth setting
    Looks like the Ember component has a slightly different behavior?

--- packages/react/tests/server.test.tsx
+++ packages/react/tests/server.test.tsx
@@ -1,4 +1,8 @@
-import { type ImageData } from '@responsive-image/core';
+import {
+  setConfig,
+  type EnvConfig,
+  type ImageData,
+} from '@responsive-image/core';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { describe, expect, it } from 'vitest';
@@ -9,6 +13,11 @@ describe('environment', () => {
   it('runs on server', () => {
     expect(typeof window).toBe('undefined');
   });
+});
+
+// Test for #1558
+setConfig<EnvConfig>('env', {
+  fallbackScreenWidth: 1280,
 });
 
 describe('ResponsiveImage', () => {
@@ -23,7 +32,7 @@ describe('ResponsiveImage', () => {
   it('renders a responsive image', () => {
     const string = renderToString(<ResponsiveImage src={defaultImageData} />);
     expect(string).toMatchInlineSnapshot(
-      `"<picture><source srcSet="/provider/w640/image.avif 640w, /provider/w750/image.avif 750w, /provider/w828/image.avif 828w, /provider/w1080/image.avif 1080w, /provider/w1200/image.avif 1200w, /provider/w1920/image.avif 1920w, /provider/w2048/image.avif 2048w, /provider/w3840/image.avif 3840w" type="image/avif"/><source srcSet="/provider/w640/image.webp 640w, /provider/w750/image.webp 750w, /provider/w828/image.webp 828w, /provider/w1080/image.webp 1080w, /provider/w1200/image.webp 1200w, /provider/w1920/image.webp 1920w, /provider/w2048/image.webp 2048w, /provider/w3840/image.webp 3840w" type="image/webp"/><source srcSet="/provider/w640/image.jpeg 640w, /provider/w750/image.jpeg 750w, /provider/w828/image.jpeg 828w, /provider/w1080/image.jpeg 1080w, /provider/w1200/image.jpeg 1200w, /provider/w1920/image.jpeg 1920w, /provider/w2048/image.jpeg 2048w, /provider/w3840/image.jpeg 3840w" type="image/jpeg"/><img class="ri-img ri-responsive" loading="lazy" decoding="async" width="3840" height="2560" src="/provider/w3840/image.jpeg"/></picture>"`,
+      `"<picture><source srcSet="/provider/w640/image.avif 640w, /provider/w750/image.avif 750w, /provider/w828/image.avif 828w, /provider/w1080/image.avif 1080w, /provider/w1200/image.avif 1200w, /provider/w1920/image.avif 1920w, /provider/w2048/image.avif 2048w, /provider/w3840/image.avif 3840w" type="image/avif"/><source srcSet="/provider/w640/image.webp 640w, /provider/w750/image.webp 750w, /provider/w828/image.webp 828w, /provider/w1080/image.webp 1080w, /provider/w1200/image.webp 1200w, /provider/w1920/image.webp 1920w, /provider/w2048/image.webp 2048w, /provider/w3840/image.webp 3840w" type="image/webp"/><source srcSet="/provider/w640/image.jpeg 640w, /provider/w750/image.jpeg 750w, /provider/w828/image.jpeg 828w, /provider/w1080/image.jpeg 1080w, /provider/w1200/image.jpeg 1200w, /provider/w1920/image.jpeg 1920w, /provider/w2048/image.jpeg 2048w, /provider/w3840/image.jpeg 3840w" type="image/jpeg"/><img class="ri-img ri-responsive" loading="lazy" decoding="async" width="1280" height="853" src="/provider/w1280/image.jpeg"/></picture>"`,
     );
   });
 
@@ -36,7 +45,7 @@ describe('ResponsiveImage', () => {
     };
     const string = renderToString(<ResponsiveImage src={imageData} />);
     expect(string).toMatchInlineSnapshot(
-      `"<picture><source srcSet="/provider/w640/image.avif 640w, /provider/w750/image.avif 750w, /provider/w828/image.avif 828w, /provider/w1080/image.avif 1080w, /provider/w1200/image.avif 1200w, /provider/w1920/image.avif 1920w, /provider/w2048/image.avif 2048w, /provider/w3840/image.avif 3840w" type="image/avif"/><source srcSet="/provider/w640/image.webp 640w, /provider/w750/image.webp 750w, /provider/w828/image.webp 828w, /provider/w1080/image.webp 1080w, /provider/w1200/image.webp 1200w, /provider/w1920/image.webp 1920w, /provider/w2048/image.webp 2048w, /provider/w3840/image.webp 3840w" type="image/webp"/><source srcSet="/provider/w640/image.jpeg 640w, /provider/w750/image.jpeg 750w, /provider/w828/image.jpeg 828w, /provider/w1080/image.jpeg 1080w, /provider/w1200/image.jpeg 1200w, /provider/w1920/image.jpeg 1920w, /provider/w2048/image.jpeg 2048w, /provider/w3840/image.jpeg 3840w" type="image/jpeg"/><img class="ri-img ri-responsive test-class" loading="lazy" decoding="async" width="3840" height="2560" src="/provider/w3840/image.jpeg"/></picture>"`,
+      `"<picture><source srcSet="/provider/w640/image.avif 640w, /provider/w750/image.avif 750w, /provider/w828/image.avif 828w, /provider/w1080/image.avif 1080w, /provider/w1200/image.avif 1200w, /provider/w1920/image.avif 1920w, /provider/w2048/image.avif 2048w, /provider/w3840/image.avif 3840w" type="image/avif"/><source srcSet="/provider/w640/image.webp 640w, /provider/w750/image.webp 750w, /provider/w828/image.webp 828w, /provider/w1080/image.webp 1080w, /provider/w1200/image.webp 1200w, /provider/w1920/image.webp 1920w, /provider/w2048/image.webp 2048w, /provider/w3840/image.webp 3840w" type="image/webp"/><source srcSet="/provider/w640/image.jpeg 640w, /provider/w750/image.jpeg 750w, /provider/w828/image.jpeg 828w, /provider/w1080/image.jpeg 1080w, /provider/w1200/image.jpeg 1200w, /provider/w1920/image.jpeg 1920w, /provider/w2048/image.jpeg 2048w, /provider/w3840/image.jpeg 3840w" type="image/jpeg"/><img class="ri-img ri-responsive test-class" loading="lazy" decoding="async" width="1280" height="853" src="/provider/w1280/image.jpeg"/></picture>"`,
     );
   });
 });
