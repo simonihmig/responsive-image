56d784d9ac52c698b72b018f642da872a44f4fb7
Simon Ihmig <simon.ihmig@kaliber5.de>
2025-09-06 22:15:23 +0200

    Run blueprint

---
+++ packages/my-addon/tests/test-helper.ts
@@ -0,0 +1,34 @@
+import EmberApp from '@ember/application';
+import Resolver from 'ember-resolver';
+import EmberRouter from '@ember/routing/router';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
+
+class Router extends EmberRouter {
+  location = 'none';
+  rootURL = '/';
+}
+
+class TestApp extends EmberApp {
+  modulePrefix = 'test-app';
+  Resolver = Resolver.withModules({
+    'test-app/router': { default: Router },
+    // add any custom services here
+  });
+}
+
+Router.map(function () {});
+
+export function start() {
+  setApplication(
+    TestApp.create({
+      autoboot: false,
+      rootElement: '#ember-testing',
+    }),
+  );
+  setup(QUnit.assert);
+  setupEmberOnerrorValidation();
+  qunitStart();
+}
