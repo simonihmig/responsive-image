ea327a9ea503990d293491de1bb2894470e390aa
Simon Ihmig <simon.ihmig@kaliber5.de>
2025-09-08 21:43:34 +0200

    Fix type import

--- apps/ember-webpack/types/global.d.ts
+++ apps/ember-webpack/types/global.d.ts
@@ -9,7 +9,7 @@ declare module 'ember-webpack/templates/*' {
 // We cannot use something like `*.jpg*` that works with queries, as TS only supports a single wildcard.
 // See https://github.com/microsoft/TypeScript/issues/38638
 declare module '*responsive' {
-  import { ImageData } from '@responsive-image/ember';
+  import { ImageData } from '@responsive-image/core';
   const value: ImageData;
   export default value;
 }
