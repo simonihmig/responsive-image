a7d9c93e573c5da4594e4687f37e7ccb94dfca2e
Simon Ihmig <simon.ihmig@kaliber5.de>
2026-02-25 23:22:30 +0100

    Add image demo

--- apps/nuxt/app/app.vue
+++ apps/nuxt/app/app.vue
@@ -1,6 +1,99 @@
+<script setup lang="ts">
+import { ResponsiveImage } from '@responsive-image/vue';
+import {
+  cloudinary,
+  fastly,
+  imgix,
+  netlify,
+  type Config,
+} from '@responsive-image/cdn';
+import image from './images/aurora.jpg?responsive';
+import imageLqipColor from './images/aurora.jpg?lqip=color&responsive';
+import imageLqipInline from './images/aurora.jpg?lqip={"type":"inline","targetPixels":16}&responsive';
+import imageLqipBlurhash from './images/aurora.jpg?lqip={"type":"blurhash","targetPixels":16}&responsive';
+import imageLqipThumbhash from './images/aurora.jpg?lqip={"type":"thumbhash"}&responsive';
+import imagePortrait from './images/aurora.jpg?aspect=2:3&responsive';
+import imageGray from './images/aurora.jpg?grayscale&responsive';
+import { setConfig } from '@responsive-image/core';
+
+setConfig<Config>('cdn', {
+  cloudinary: {
+    cloudName: 'responsive-image',
+  },
+  fastly: {
+    domain: 'www.fastly.io',
+  },
+  imgix: {
+    domain: 'responsive-image.imgix.net',
+  },
+  netlify: {
+    domain: 'responsive-image.dev',
+  },
+});
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+
+    <h1>ResponsiveImage for Vue</h1>
+
+    <h2>Netlify</h2>
+    <ResponsiveImage
+      :src="netlify('aurora-original.jpg', { aspectRatio: 1.4971927636 })"
+      data-test-netlify-image
+    />
+    <h2>Cloudinary</h2>
+    <ResponsiveImage
+      :src="
+        cloudinary('aurora-original_w0sk6h', {
+          aspectRatio: 1.4971927636,
+        })
+      "
+      data-test-cloudinary-image
+    />
+    <h2>Fastly</h2>
+    <ResponsiveImage
+      :src="fastly('image.webp', { aspectRatio: 2 })"
+      data-test-fastly-image
+    />
+    <h2>imgix</h2>
+    <ResponsiveImage
+      :src="imgix('aurora-original.jpg', { aspectRatio: 1.4971927636 })"
+      data-test-imgix-image
+    />
+    <h2>Local</h2>
+    <ResponsiveImage :src="image" data-test-local-image="responsive" />
+    <ResponsiveImage :src="image" :width="320" data-test-local-image="fixed" />
+    <ResponsiveImage
+      :src="imageLqipColor"
+      :width="320"
+      data-test-local-image="fixed,lqip-color"
+    />
+    <ResponsiveImage
+      :src="imageLqipInline"
+      :width="320"
+      data-test-local-image="fixed,lqip-inline"
+    />
+    <ResponsiveImage
+      :src="imageLqipBlurhash"
+      :width="320"
+      data-test-local-image="fixed,lqip-blurhash"
+    />
+    <ResponsiveImage
+      :src="imageLqipThumbhash"
+      :width="320"
+      data-test-local-image="fixed,lqip-thumbhash"
+    />
+    <ResponsiveImage
+      :src="imageGray"
+      :width="320"
+      data-test-local-image="fixed,grayscale"
+    />
+    <ResponsiveImage
+      :src="imagePortrait"
+      :width="320"
+      data-test-local-image="fixed,aspect"
+    />
   </div>
 </template>
--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,5 +1,14 @@
+import { setupPlugins } from '@responsive-image/vite-plugin';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: false },
+  vite: {
+    plugins: [
+      setupPlugins({
+        include: /^[^?]+\.jpg\?.*responsive.*$/,
+      }),
+    ],
+  },
+});
