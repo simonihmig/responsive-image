0635e3198a672781b387a104dddafaed09992b9d
Simon Ihmig <simon.ihmig@kaliber5.de>
2026-03-16 22:36:10 +0100

    Make client type declarations more specific

--- packages/vite-plugin/client.d.ts
+++ packages/vite-plugin/client.d.ts
@@ -1,7 +1,13 @@
 // To make TS understand our image imports, we have to tag them using a `responsive` query parameter, that has to come *last*!
 // We cannot use something like `*.jpg*` that works with queries, as TS only supports a single wildcard.
 // See https://github.com/microsoft/TypeScript/issues/38638
-declare module '*responsive' {
+declare module '*&responsive' {
+  import type { ImageData } from '@responsive-image/core';
+  const value: ImageData;
+  export default value;
+}
+
+declare module '*?responsive' {
   import type { ImageData } from '@responsive-image/core';
   const value: ImageData;
   export default value;
