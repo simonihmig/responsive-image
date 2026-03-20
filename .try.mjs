ecbe60773920c011c693cc553e00de092a97bf6d
Simon Ihmig <simon.ihmig@kaliber5.de>
2026-03-20 12:11:08 +0100

    Make try setup work for all packages

--- .try.mjs
+++ .try.mjs
@@ -2,6 +2,7 @@ export default {
   scenarios: [
     {
       name: `ember-lts-6.4`,
+      cwd: 'packages/ember',
       npm: {
         devDependencies: {
           'ember-source': `npm:ember-source@~6.4.0`,
@@ -10,6 +11,7 @@ export default {
     },
     {
       name: `ember-lts-6.8`,
+      cwd: 'packages/ember',
       npm: {
         devDependencies: {
           'ember-source': `npm:ember-source@~6.8.0`,
@@ -18,6 +20,7 @@ export default {
     },
     {
       name: `ember-beta`,
+      cwd: 'packages/ember',
       npm: {
         devDependencies: {
           'ember-source': `npm:ember-source@beta`,
