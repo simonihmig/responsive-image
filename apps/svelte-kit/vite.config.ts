a4128bcc6730176fe7d4ccc5c8d93862f2ccf9e3
Simon Ihmig <simon.ihmig@kaliber5.de>
2026-03-21 23:43:52 +0100

    Ignore type errors

--- apps/svelte-kit/vite.config.ts
+++ apps/svelte-kit/vite.config.ts
@@ -3,7 +3,11 @@ import { responsiveImage } from '@responsive-image/vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), responsiveImage()],
+  plugins: [
+    sveltekit(),
+    // @ts-expect-error -- https://github.com/vitejs/vite/issues/21981
+    responsiveImage(),
+  ],
   preview: {
     port: 4202,
   },
