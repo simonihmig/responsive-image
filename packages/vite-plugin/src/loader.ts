c285f607c4e5698616c62139bafcc979f8add551
Simon Ihmig <simon.ihmig@kaliber5.de>
2026-03-23 20:37:42 +0100

    Use roldown hook filters

--- packages/vite-plugin/src/loader.ts
+++ packages/vite-plugin/src/loader.ts
@@ -1,5 +1,5 @@
 import { normalizeInput } from '@responsive-image/build-utils';
-import { createFilter } from '@rollup/pluginutils';
+import { createFilter } from 'vite';
 
 import { getViteOptions, META_KEY } from './utils';
 
@@ -10,28 +10,38 @@ export default function loaderPlugin(
   userOptions: Partial<Options> = {},
 ): Plugin {
   const options = getViteOptions('', userOptions);
-  const filter = createFilter(options.include, options.exclude);
+  const { include, exclude } = options;
+  const filter = createFilter(include, exclude);
 
   return {
     name: 'responsive-image/loader',
     // TODO do we need this?
     enforce: 'pre',
-    async load(id) {
-      if (!filter(id)) {
-        return;
-      }
-
-      const options = getViteOptions(id, userOptions);
-      const data = normalizeInput(id, { generateHash: options.cache });
-
-      return {
-        // Only the export plugin will actually return ESM code
-        code: '',
-        moduleSideEffects: false,
-        meta: {
-          [META_KEY]: data,
+    load: {
+      // Filter on the Rust-side when used with rolldown: https://rolldown.rs/apis/plugin-api/hook-filters#plugin-hook-filters
+      filter: {
+        id: {
+          include,
+          exclude,
         },
-      };
+      },
+      async handler(id) {
+        if (!filter(id)) {
+          return;
+        }
+
+        const options = getViteOptions(id, userOptions);
+        const data = normalizeInput(id, { generateHash: options.cache });
+
+        return {
+          // Only the export plugin will actually return ESM code
+          code: '',
+          moduleSideEffects: false,
+          meta: {
+            [META_KEY]: data,
+          },
+        };
+      },
     },
   };
 }
