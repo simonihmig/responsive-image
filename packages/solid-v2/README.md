8fca5821036947f6af66b2a77c07104b1bc094db
Roel Bruggink <roel@jaroel.nl>
2026-08-31 19:51:56 +0200

    Add solidjsv2 package

---
+++ packages/solid-v2/README.md
@@ -0,0 +1,17 @@
+# @responsive-image/solid-v2
+
+Solid.js v2 component to render responsive images, provided as locally processed images or loaded remotely from Image CDNs.
+
+Part of the [ResponsiveImage](https://github.com/simonihmig/responsive-image) project.
+
+## Compatibility
+
+- Solid 2.0 or above (RC)
+
+## Documentation
+
+Visit [responsive-image.dev](https://responsive-image.dev) for our complete documentation site.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
---
+++ packages/solid-v2/vitest.config.ts
@@ -0,0 +1,53 @@
+import solidPlugin from '@solidjs/vite-plugin';
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig(({ mode }) => {
+  // to test in server environment, run with "--mode ssr" or "--mode test:ssr" flag
+  // loads only server.test.ts file
+  const testSSR = mode === 'test:ssr' || mode === 'ssr';
+
+  return {
+    plugins: [
+      solidPlugin({
+        // https://github.com/solidjs/solid-refresh/issues/29
+        hot: false,
+        // For testing SSR we need to do a SSR JSX transform
+        solid: { generate: testSSR ? 'ssr' : 'dom' },
+      }),
+    ],
+    test: {
+      browser: {
+        enabled: !testSSR,
+        provider: playwright({ launchOptions: { channel: 'chrome' } }),
+        instances: [
+          {
+            name: 'Chrome',
+            browser: 'chromium',
+          },
+        ],
+      },
+      watch: false,
+      isolate: !testSSR,
+      env: {
+        NODE_ENV: testSSR ? 'production' : 'development',
+        DEV: testSSR ? '' : '1',
+        SSR: testSSR ? '1' : '',
+        PROD: testSSR ? '1' : '',
+      },
+      transformMode: { web: [/\.[jt]sx$/] },
+      ...(testSSR
+        ? {
+            include: ['tests/server.test.{ts,tsx}'],
+          }
+        : {
+            include: ['tests/*.test.{ts,tsx}'],
+            exclude: ['tests/server.test.{ts,tsx}'],
+          }),
+      ...(testSSR ? { environment: 'node' } : {}),
+    },
+    resolve: {
+      conditions: testSSR ? ['node'] : ['browser', 'development'],
+    },
+  };
+});
